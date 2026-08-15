4b36c7bcf32d273394d3c1d8b2ab95da851276d9
ParsaAzima <parsaazima489@gmail.com>
2026-08-15 13:55:47 +0330

    Created kick handler

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from telebot import TeleBot
 
 from groups.ban import ban_handler
 from groups.unban import unban_handler
-
+from groups.kick import kick_handler
 
 bot = TeleBot("TOKEN")
 
 ban_handler(bot)
 unban_handler(bot)
+kick_handler(bot)
 
 bot.infinity_polling()
